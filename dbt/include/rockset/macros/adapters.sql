6676d671e223f856239ac25d137031c710d476b5
Sam Crowder <sam@rockset.com>
2021-10-11 04:07:10 +0000

    make source macro work gs

--- dbt/include/rockset/macros/adapters.sql
+++ dbt/include/rockset/macros/adapters.sql
@@ -1,15 +1,9 @@
-{% macro rockset__alter_column_type(relation, column_name, new_column_type) -%}
-  {% call statement('alter_column_type') %}
-    INSERT INTO {{ relation }} ( "_id", {{ column_name }} )
-    SELECT
-      "_id",
-      CAST( {{ column_name }} as {{ new_column_type }} ) as {{ column_name }}
-    FROM {{ relation }}
-  {%- endcall %}
-{% endmacro %}
-
--- Rockset does not have a notion of database, so do not include it when resolving refs from other models
+-- Rockset does not have a notion of database, so do not include it when resolving refs or sources
 -- It resolves as schema.identifier (i.e. workspace.collection), instead of database.schema.identifier
 {% macro ref(modelname) %}
   {{ builtins.ref(modelname).include(database=False).render() }}
+{% endmacro %}
+
+{% macro source(source_name, table_name) %}
+  {{ builtins.source(source_name, table_name).include(database=False).render() }}
 {% endmacro %}
