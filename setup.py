afb354b3377fcb75f8ac2bc6e172538cbeb59d0f
Julius Hochmuth <julius@rockset.com>
2022-12-08 15:28:12 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from distutils.core import setup
 
 package_name = "dbt-rockset"
-package_version = "0.1.5"
+package_version = "0.1.6"
 description = """The Rockset adapter plugin for dbt (data build tool)"""
 
 setup(
