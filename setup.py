6dc840500aeb351cfa64a77fdd8a2c80aa8d75f8
Steven Baldwin <sbaldwin@rockset.com>
2024-03-22 13:59:35 -0700

    Fix install dependencies

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ setup(
     long_description=description,
     author="Steven Baldwin",
     author_email="sbaldwin@rockset.com",
-    url="If you have already made a github repo to tie the project to place it here, otherwise update in setup.py later.",
+    url="https://github.com/rockset/dbt-rockset",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        "dbt-core~=1.7.0.",
+        "dbt-core~=1.7.0",
+        "rockset_sqlalchemy~=0.0.1"
     ],
 )
