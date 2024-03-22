ee4f808e3f640b6746d5f275edd27aec00f97bcb
Steven Baldwin <sbaldwin@rockset.com>
2024-03-22 14:20:01 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 
 package_name = "dbt-rockset"
 # make sure this always matches dbt/adapters/{adapter}/__version__.py
-package_version = "1.7.0"
+package_version = "1.7.1"
 description = """The Rockset adapter plugin for dbt"""
 
 setup(
